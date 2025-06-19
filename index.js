80ad3e41d9cce11fed2748cbbe18ffb6ffe2a9a8
kurui <kuruijoshua@gmail.com>
2025-06-19 10:54:17 +0300

    Dom nodes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main id="main"> element
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id to "victory"
+newHeader.id = "victory";
+
+// Set the text content
+newHeader.textContent = "Kurui is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
